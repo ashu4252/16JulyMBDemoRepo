b4d72496ef67c720ca34b396eae57900d1869f4f
ashu4252 <ashusharma9572@gmail.com>
2022-12-19 09:07:49 +0530

    Basic Commit
    Hi All,
    I have created project on github.com go head and clone it, start with your respective tasks........

---
+++ SampleProject/src/pageLayer/TestBase.java
@@ -0,0 +1,13 @@
+package pageLayer;
+
+public class TestBase {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Hello World");
+		
+		System.out.println("TestBase");
+		System.out.println("Added dependied in pom.xml");
+	}
+
+}
